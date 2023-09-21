29f4b2883e697aeec92286d3dcb751840a781944
bafflingbits <143870286+bafflingbits@users.noreply.github.com>
2023-09-21 10:33:23 +0000

    A complete graph will fail the new checks, so change checkCounterExample to try to randomly find a graph that passes the checks.

--- checkCounterExample.py
+++ checkCounterExample.py
@@ -3,7 +3,10 @@ import time
 import os
 import glob
 from collections import defaultdict
-from itertools import product
+import itertools
+
+from subgraphChecks import checkSubgraphConstraints
+
 
 def get_perfect_matchings(edge_list):
     def get_edges_of_vertices(edge_list, vertices):
@@ -11,14 +14,11 @@ def get_perfect_matchings(edge_list):
 
     matchings_set = set()
 
-    n = len(edge_list)
-    for i in range(n):
-        edge1 = tuple(edge_list[i])
+    for edge1 in edge_list:
         subgraph1 = get_edges_of_vertices(edge_list, set(range(6)) - set(edge1[:2]))
-        for j in range(len(subgraph1)):
-            edge2 = tuple(subgraph1[j])
+        for edge2 in subgraph1:
             subgraph2 = get_edges_of_vertices(subgraph1, set(range(6)) - set(edge1[:2]) - set(edge2[:2]))
-            for edge3 in subgraph2:  
+            for edge3 in subgraph2:
                 # Sorting the edges to avoid permutations being considered as separate matchings
                 matching = tuple(sorted([edge1, edge2, tuple(edge3)]))
                 matchings_set.add(matching)
@@ -38,7 +38,7 @@ def get_ivc(perfect_matching):
 def count_ivc(perfect_matchings):
     # Initialize the counts to 0 for all possible IVCs
     all_possible_ivcs = defaultdict(int)
-    for ivc in product(range(3), repeat=6):
+    for ivc in itertools.product(range(3), repeat=6):
         all_possible_ivcs[ivc] = 0
 
     # Count the IVCs in the given perfect matchings
@@ -59,7 +59,7 @@ def ivc_conditions(edges):
     result = count_ivc(perfect_matchings)
     for ivc, count in result.items():
         #print(f"IVC {ivc} appears {count} times.")
-        
+
         if len(set(ivc))==1:
             # monochromatic IVC needs to exist at least once:
             if count==0:
@@ -70,93 +70,138 @@ def ivc_conditions(edges):
             if count==1:
                 #print(f"Non-Monochromatic IVC with color={ivc} exist exactly once.")
                 return False
-    
+
     return True
+
+
+def checkGraph(vertices, colors, edges):
+    if not ivc_conditions(edges):
+        return False
+    return checkSubgraphConstraints(vertices, colors, edges)
+
+
+def randomGraph(full_edge_set, edge_probability):
+    edges = set()
+    for edge in full_edge_set:
+        if random.random() < edge_probability:
+            edges.add(edge)
+    return edges
+
+def randomAddEdges(edges, n, full_edge_set):
+    # Note: modifies edges set in place
+    missing_edges = full_edge_set - edges
+    if len(missing_edges) < n:
+        edges.update(missing_edges)
+    else:
+        subset = random.sample(list(missing_edges), n)
+        edges.update(subset)
+
+def randomRemoveEdges(edges, n):
+    # Note: modifies edges set in place
+    if len(edges) < n:
+        edges = set()
+    else:
+        subset = random.sample(list(edges), n)
+        edges.difference_update(subset)
+
 
 directory="results"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-    
+
 while True:
     vertices = list(range(6))
     colors = list(range(3))
-    
+
+    all_edges = set()
+    for n1,n2 in itertools.combinations(vertices, 2):
+        for c1,c2 in itertools.product(colors, repeat=2):
+            all_edges.add((n1,n2,c1,c2))
+
     CURR_ID=random.randint(10000000, 99999999)
-    
+
     all_min_graphs=[]
-    
+
     min_graph=[]
     min_graph_len=666
-    
+
     while True:
-        if random.random()<0.0001:
-            print("Start completely from scratch")
-        # Create complete Graph
-        if len(min_graph)==0:
-            edges = []
-            for i in vertices:
-                for j in range(i + 1, len(vertices)):
-                    for colA in colors:
-                        for colB in colors:
-                            edge = [i, j, colA, colB]
-                            edges.append(edge)
+
+        # first try to randomly generate a graph that passes all the checks
+        attempts = 1
+        edges = randomGraph(all_edges, 0.5)
+        while not checkGraph(vertices, colors, edges):
+            attempts +=1
+            if attempts % 100 == 0:
+                print(f"... {attempts} attempts")
+            if random.random() < 0.0001:
+                print("Start completely from scratch")
+                edges = randomGraph(all_edges, 0.5)
+            elif random.random() < 0.5:
+                randomAddEdges(edges, random.randint(1,5), all_edges)
+            else:
+                randomRemoveEdges(edges, random.randint(1,5))
+        print(f"Found a graph in {attempts} attempts")
+
+        # -- previously, sometimes start from a known good graph?
+        # turn that off for now
+        """
         else:
             if random.random()>0.2:
-                edges=min_graph[:] 
+                edges=min_graph[:]
             else:
                 edges=random.choice(all_min_graphs)[:]
-        
-        
+
         res=True
-        if min_graph_len!=666 and ivc_conditions(min_graph)==False:
+        if min_graph_len!=666 and checkGraph(vertices, colors, edges) == False:
             print("MISTAKE")
             for _ in range(10):
                 time.sleep(1)
-                
+        """
+
+        # try to randomly minimize the graph
         while True:
-            
+
             if len(edges)<min_graph_len:
-                min_graph = edges[:]
+                min_graph = list(edges)
                 min_graph_len=len(edges)
                 #print(min_graph)
                 #print(f'Current minimum length: {min_graph_len}')
-                
+
                 path = os.path.join("results", f"solution{len(min_graph)}_{CURR_ID}.txt")
                 did_write=False
                 while not did_write:
                     try:
                         with open(path, 'w') as file:
-                            file.write(f"{min_graph}")  
+                            file.write(f"{min_graph}")
                         pattern = os.path.join(directory, f"solution*_{CURR_ID}.txt")
-                        
+
                         # Get a list of matching filenames
                         files_to_check = glob.glob(pattern)
-                        
+
                         for file in files_to_check:
                             # Extract the XXX number from the filename
                             number = int(file.split("solution")[1].split("_")[0])
-                            
+
                             # Check if it's not the length of min_graph
                             if number != len(min_graph):
                                 os.remove(file)
-    
+
                         did_write=True
                     except:
                         time.sleep(0.1)
-    
+
                 all_min_graphs.append(min_graph)
-    
+
             else:
-                #print(f'    Current length: {len(edges)} (best: {min_graph_len})')      
+                #print(f'    Current length: {len(edges)} (best: {min_graph_len})')
                 pass
-            
+
             num_elements=random.choice([1,2,3,4])
-            for _ in range(num_elements):
-                random_element = random.choice(edges)
-                edges.remove(random_element)
-        
-            res=ivc_conditions(edges)
+            randomRemoveEdges(edges, num_elements)
+
+            res = checkGraph(vertices, colors, edges)
             if not res:
                 break
-                
+
